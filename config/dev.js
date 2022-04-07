dad82d52eb61cfe672d97139613ff18a3b37184c
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-04-07 10:50:08 -0500

    finished mongo set up

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://rohanramachandr:1lWDL29zvLBKvBoy@cluster0-shard-00-00.abae6.mongodb.net:27017,cluster0-shard-00-01.abae6.mongodb.net:27017,cluster0-shard-00-02.abae6.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-e6op4w-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
