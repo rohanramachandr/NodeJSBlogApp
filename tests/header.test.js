eaf1ae7ea12e4d49f564d14c7f8ba4a24fb5d718
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-04-10 15:49:27 -0500

    started testing

---
+++ tests/header.test.js
@@ -0,0 +1,38 @@
+const { APIGateway } = require('aws-sdk');
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+            headless: false
+        });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await browser.close();
+});
+
+test('header has correct text', async () => {
+
+    
+
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+
+
+
+});
+
+test('clicking login starts oauth flow', async () => {
+    await page.click('.right a');
+
+    const url = await page.url();
+
+    expect(url).toMatch(/accounts\.google\.com/)
+
+
+});
