4e47232a8df8273614de060e18074cbc1b5e7cb0
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-04-12 10:20:56 -0500

    writing blog tests

---
+++ tests/blogs.test.js
@@ -0,0 +1,24 @@
+const Page = require('./helpers/page');
+Number.prototype._called = {};
+
+let page;
+
+beforeEach(async () => {
+    page= await Page.build();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await page.close();
+});
+
+test('When logged in, can see blog create form', async() => {
+    await page.login();
+    await page.click('a.btn-floating');
+
+    const label = await page.getContentsOf('form label');
+
+    expect(label).toEqual('Blog Title');
+
+    
+});
--- tests/header.test.js
+++ tests/header.test.js
@@ -1,25 +1,25 @@
-const { APIGateway } = require('aws-sdk');
-const puppeteer = require('puppeteer');
 
-let browser, page;
+
+const Page = require('./helpers/page');
+Number.prototype._called = {};
+
+let  page;
 
 beforeEach(async () => {
-    browser = await puppeteer.launch({
-            headless: false
-        });
-    page = await browser.newPage();
+    
+    page = await Page.build();
     await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-    await browser.close();
+    await page.close();
 });
 
 test('header has correct text', async () => {
-
+    await page.login();
     
 
-    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    const text = await page.getContentsOf('a.brand-logo');
 
     expect(text).toEqual('Blogster');
 
@@ -33,6 +33,24 @@ test('clicking login starts oauth flow', async () => {
     const url = await page.url();
 
     expect(url).toMatch(/accounts\.google\.com/)
+
+
+});
+
+test('When signed in, shows logout button', async () => {
+
+    await page.login();
+
+ 
+    
+
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+
+    expect(text).toEqual('Logout');
+
+
+
 
 
 });
