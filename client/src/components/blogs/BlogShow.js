93aa752ea4c3bc0dd6d7def474a71bb4c4c64dd3
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-04-26 10:36:53 -0500

    finished image upload

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -7,6 +7,14 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+
+      return <img src={'https://my-blog-bucket-1999.s3.amazonaws.com/' + this.props.blog.imageUrl}/>
+
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +26,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,6 +1,25 @@
+const AWS = require('aws-sdk');
+const keys = require('../config/keys');
+const uuid = require('uuid/v1');
+const requireLogin = require('../middlewares/requireLogin');
+
+const s3 = new AWS.S3({
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey
+});
+
 module.exports = app => {
 
-    app.get('/api/upload', (req,res) => {
+    app.get('/api/upload', requireLogin, (req,res) => {
+
+        const key = `${req.user.id}/${uuid()}.jpeg`;
+
+        s3.getSignedUrl('putObject', {
+            Bucket: 'my-blog-bucket-1999',
+            ContentType: 'image/jpeg',
+            Key: key
+
+        }, (err, url) => res.send({ key, url }));
 
     });
 
